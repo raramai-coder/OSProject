6f747de857a5044ac1c1778c2a7d0f43c24816c8
raramai-coder <1846409@students.wits.ac.za>
2022-09-15 15:29:43 +0000

    added project files

---
+++ witsshell.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+
+int main(int MainArgc, char *MainArgv[]){
+	
+	bool run =true;
+	char input[] = {};
+	
+	while (run)
+	{
+		printf("witsshell>");
+
+	}
+	
+
+	return(0);
+}
