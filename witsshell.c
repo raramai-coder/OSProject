d9763257b7980b1503e95d2e8ba755c9fda4d95d
raramai-coder <1846409@students.wits.ac.za>
2022-09-15 18:38:31 +0000

    input string and exit

--- witsshell.c
+++ witsshell.c
@@ -7,14 +7,32 @@
 #include <assert.h>
 #include <fcntl.h>
 
+
 int main(int MainArgc, char *MainArgv[]){
 	
 	bool run =true;
 	char input[] = {};
+	char exit[] = "exit\n";
+
+	//variables for getline function
+	char buffer[32];
+    char *b = buffer;
+    size_t bufsize = 32;
+    size_t characters;
+
 	
 	while (run)
 	{
-		printf("witsshell>");
+		
+		
+    	printf("witsshell>");
+    	characters = getline(&b,&bufsize,stdin);
+
+		int comp = strcmp(buffer,exit);
+
+		if(comp==0){
+			run = false; 
+		}
 
 	}
 	
