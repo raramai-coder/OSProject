a9b73f59821694bce75277973268e4b30df2a0d0
raramai-coder <1846409@students.wits.ac.za>
2022-09-15 21:55:11 +0000

    add input to command array

--- witsshell.c
+++ witsshell.c
@@ -8,11 +8,56 @@
 #include <fcntl.h>
 
 
+#pragma region linked-list-structure
+
+struct node {
+   int data;
+   int key;
+   struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+//display the list
+void printList() {
+   struct node *ptr = head;
+   printf("\n[ ");
+	
+   //start from the beginning
+   while(ptr != NULL) {
+      printf("(%d,%d) ",ptr->key,ptr->data);
+      ptr = ptr->next;
+   }
+	
+   printf(" ]");
+}
+
+//insert link at the first location
+void insertFirst(int key, int data) {
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+	
+   link->key = key;
+   link->data = data;
+	
+   //point it to old first node
+   link->next = head;
+	
+   //point first to new first node
+   head = link;
+}
+#pragma endregion 
+
+
+
 int main(int MainArgc, char *MainArgv[]){
 	
 	bool run =true;
 	char input[] = {};
 	char exitCommand[] = "exit\n";
+
+	char *command[1024];
 
 	//variables for getline function
 	char buffer[32];
@@ -26,20 +71,28 @@ int main(int MainArgc, char *MainArgv[]){
     	printf("witsshell>");
     	characters = getline(&b,&bufsize,stdin);
 
-		int comp = strcmp(buffer,exitCommand);
+		int comp = strcmp(b,exitCommand);
 
 		if(comp==0){
 			
-			//run = false; 
 			exit(0);
 		}
-		printf("%u",comp);
+		//printf("%u",comp);
 		char *found;
+		int arraySize = 0;
+		while( (found = strsep(&b," ")) != NULL ){
+			//printf("%s\n",found);
+			
+			command[arraySize] = found;
+			arraySize++;
+		}
 
-		// while( (found = strsep(&b," ")) != NULL ){
-		// 	printf("%s\n",found);
-		// }
-        
+		for (int i = 0; i < arraySize; i++)
+		{
+			printf("%s\n",command[i]);
+		}
+		
+		
 
 	}
 	
